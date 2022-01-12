1961d56f9b45bf4cdefdb520adcb053109a98fb8
sidneyishikawa <65741827+sidneyishikawa@users.noreply.github.com>
2022-01-12 19:40:49 -0300

    Criação cadastro produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+function App(props) {
+
+  // state = {
+  //   nome: ''
+  // }
+  // constructor() {
+  //   super()
+  //   this.modificarNomeN = this.modificarNomeN.bind(this)
+
+  // }
+
+  // modificarNomeN(event) {
+  //   let nome = event.target.value;
+  //   this.setState({
+  //     nome: nome
+
+  //   })
+  // }
+  //modificarNome = (event) => {
+  // let nome = event.target.value;
+  // this.setState({
+  //   nome: nome
+
+  // })
+  // }
+  const modificarNome = event => {
+    console.log(event.target.value);
+  }
+
+  const criarComboBox = () => {
+    const opcoes = ["Fulano", "Ciclano"]
+    const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+  // componentDidMount() {
+  //   console.log('Executou o componente componentDidMount')
+
+  // }
+
+  // render() {
+
+  const MeuComboBox = () => criarComboBox()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input className="texto-centralizado" type="text" value={props.nome} onChange={modificarNome} />
+
+      <h2>Hello {props.nome}  {props.idade} </h2>
+      {/* <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello {this.state.nome} </h1>
+        <h2>Hello {this.props.nome}  {this.props.idade} </h2> */}
+      <MeuComboBox></MeuComboBox>
+    </>
+  )
+
+  // }
+
 }
+
+
+
+
 
 export default App;
